228f25e9d56ee0077e117b080f1f1b1e5792d658
Ник Иващенко <melancholic8078@gmail.com>
2024-08-11 22:54:46 +0300

    try1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -7,20 +7,21 @@ const lightbox = new SimpleLightbox('.gallery-list a', {
     captionsData: 'alt',
     captionDelay: 250
  });
- const list = document.querySelector(".gallery-list");
+const list = document.querySelector(".gallery-list");
 export function createImages(data) {
-
+    
     let images =  data.hits.map((hit) => `<div class="image-frame"><a href="${hit.largeImageURL}"><img class="image" src="${hit.webformatURL}" alt="${hit.tags}" /></a><div class ="text-wraper"><div class="text-block"><h5>likes</h5><p>${hit.likes}</p></div><div class="text-block"><h5>views</h5><p>${hit.views}</p></div><div class="text-block"><h5>comments</h5><p>${hit.comments}</p></div><div class="text-block"><h5>downloads</h5><p>${hit.downloads}</p></div></div></div>`).join("");
     list.insertAdjacentHTML("beforeend", images);
     lightbox.refresh();
 }
 
 export function clearImages() {
-   
+    
     list.innerHTML = "";
 }
 
 export function scrollDown() {
+  
     const height = list.firstElementChild.getBoundingClientRect().height;
     window.scrollBy({
         top: height * 2,
